6b6c5caa49218e2a6ca04fc36b593931b3590e06
redwing-381 <123386175+redwing-381@users.noreply.github.com>
2024-08-07 01:37:55 +0530

    changes in navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loading from "./components/Loading";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Home from "./views/Home";
+import Profile from "./views/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import Hero from './components/Hero';
 import Features from './components/Features';
@@ -30,6 +32,8 @@ const App = () => {
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
         <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
           <Route path="/" exact>
             <Hero />
             <Features />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,18 +55,7 @@ const NavBar = () => {
                   Home
                 </NavLink>
               </NavItem>
-              {isAuthenticated && (
-                <NavItem>
-                  <NavLink
-                    tag={RouterNavLink}
-                    to="/external-api"
-                    exact
-                    activeClassName="router-link-exact-active"
-                  >
-                    External API
-                  </NavLink>
-                </NavItem>
-              )}
+              
             </Nav>
             <Nav className="d-none d-md-block" navbar>
               {!isAuthenticated && (
