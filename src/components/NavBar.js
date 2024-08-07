97001e1dd96123e45c191e0e4b3223e516c89689
Salvius10 <melvinsalvius.i@gmail.com>
2024-08-07 11:42:20 +0530

    Block width

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,7 +41,7 @@ const NavBar = () => {
     <div className="nav-container">
       <Navbar color="light" light expand="md" container={false}>
         <Container>
-          <NavbarBrand className="logo" />
+          <NavBar className="logo" />
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="mr-auto" navbar>
