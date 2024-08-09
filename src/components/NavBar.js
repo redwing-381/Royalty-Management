c75c9bf128e88d9491389f09d5739c36d2f22b98
redwing-381 <123386175+redwing-381@users.noreply.github.com>
2024-08-09 10:58:22 +0530

    updated navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,7 +41,6 @@ const NavBar = () => {
     <div className="nav-container">
       <Navbar color="light" light expand="md" container={false}>
         <Container>
-          <NavbarBrand className="logo" />
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="mr-auto" navbar>
